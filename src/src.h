f9f201bd984e02e6363eb03f97091e64e560a7ed
phkevin <phkevin@student.42luxembourg.lu>
2024-06-12 08:49:52 +0200

    - Ajout de define couleur dans src.h

--- src/src.h
+++ src/src.h
@@ -17,6 +17,15 @@
 # include "../lib/ftprintf/ft_printf.h"
 # include "../lib/getnextline/libgetnextline.h"
 
+/* Color MSG*/
+# define NOCOLORS "\033[0m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+
 <Your fonction>
 
 #endif
