5a682cdf0fcea0544e52deb500d34c6710a7916f
Phoenixnoir <phoenixnoirdev@users.noreply.github.com>
2024-08-23 09:44:31 +0200

    Update ft_strequal.c
    Simplification du ft_strequal

--- lib/libft/ft_strequal.c
+++ lib/libft/ft_strequal.c
@@ -15,16 +15,15 @@
 /**
  * @brief Compare deux chaînes de caractères pour vérifier leur égalité.
  *
- * Cette fonction utilise `strcmp` pour comparer deux chaînes de caractères. 
- * Elle retourne 0 si les chaînes sont égales et 1 si elles sont différentes.
+ * Cette fonction utilise `ft_strcmp` pour comparer deux chaînes de caractères.
+ * Elle retourne 1 si les chaînes sont égales et 0 si elles sont différentes.
  *
  * @param str1 La première chaîne de caractères à comparer.
  * @param str2 La seconde chaîne de caractères à comparer.
- * @return int Retourne 0 si les chaînes sont égales, 1 sinon.
+ * @return int Retourne 1 si les chaînes sont égales, 0 sinon.
  */
 int	ft_strequal(const char *str1, const char *str2)
 {
-	if (ft_strcmp(str1, str2) == 0)
-		return 0;
-	return 1;
+	return (!ft_strcmp(str1, str2));
 }
+
